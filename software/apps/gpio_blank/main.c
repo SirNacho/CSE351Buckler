a099eacfdd38f799e440babadf740381dc63425c
Sir Nacho <steven.frausto@nachoweb.tech>
2025-09-29 03:03:05 -0500

    Added codes from last thursday lab activity

--- software/apps/gpio_blank/main.c
+++ software/apps/gpio_blank/main.c
@@ -1,12 +1,3 @@
-// Blink app
-//
-// Blinks the LEDs on Buckler
-
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-
-#include "app_error.h"
 #include "nrf.h"
 #include "nrf_delay.h"
 #include "nrf_log.h"
@@ -26,8 +17,36 @@ int main(void) {
   APP_ERROR_CHECK(error_code);
   NRF_LOG_DEFAULT_BACKENDS_INIT();
   printf("Log initialized!\n");
+  
+  uint32_t* outSetPtr = (uint32_t *) 0x50000508;
+  uint32_t* outClrPtr = (uint32_t *) 0x5000050C;
+  uint32_t* cnfPtr = (uint32_t *) 0x50000700;
+  
+  //printf("%X\n%X\n%X\n", outSetPtr, outClrPtr, cnfPtr);
+
+  uint32_t* gpio23Ptr = (uint32_t *) 0x5000075C;
+  uint32_t* gpio24Ptr = (uint32_t *) 0x50000760;
+  uint32_t* gpio25Ptr = (uint32_t *) 0x50000764;
+
+  *gpio23Ptr = 1;
+  *gpio24Ptr = 1;
+  *gpio25Ptr = 1;
 
   // loop forever
   while (1) {
+
+    *outClrPtr = 1<<25;
+    
+    nrf_delay_ms(500);
+
+    // *outSetPtr = 1 << 25;
+    // nrf_delay_ms(500);
+    // *outSetPtr = 1 << 24; 
+    //sleep(3);
+    // *outClrPtr = 1 << 8;
+
+    //*gpio23Ptr = 1<<8;
+    //*gpio24Ptr = 1<<8;
+    //*gpio25Ptr = 1<<8;
   }
 }
